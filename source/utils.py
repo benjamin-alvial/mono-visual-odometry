ef855af559af18de106aac45461fbd37af7c7dd1
benjamin-alvial <117480317+benjamin-alvial@users.noreply.github.com>
2024-12-14 00:05:36 -0300

    feat: support GFTT and SuperPoint

--- source/utils.py
+++ source/utils.py
@@ -6,21 +6,26 @@ import numpy as np
 # @param frame Frame to be used for drawing (will be converted to RGB).
 # @param prevPts Previous frame keypoints.
 # @param currPts Next frame keypoints.
-def drawFrameFeatures(frame, prevPts, currPts, frameIdx):
+def drawFrameFeatures(frame, prevPtsGFTT, currPtsGFTT, prevPtsSP, currPtsSP, frameIdx):
     currFrameRGB = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
-    for i in range(len(currPts)-1):
-        cv2.circle(currFrameRGB, (int(currPts[i][0]),int(currPts[i][1])), radius=3, color=(200, 100, 0))
-        cv2.line(currFrameRGB, (int(prevPts[i][0]),int(prevPts[i][1])), (int(currPts[i][0]),int(currPts[i][1])), color=(200, 100, 0))
-        cv2.putText(currFrameRGB, "Frame: {}".format(frameIdx), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200))
-        cv2.putText(currFrameRGB, "Features: {}".format(len(currPts)), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200))    
+    for i in range(len(currPtsGFTT)-1):
+        cv2.circle(currFrameRGB, (int(currPtsGFTT[i][0]),int(currPtsGFTT[i][1])), radius=3, color=(200, 100, 0))
+        cv2.line(currFrameRGB, (int(prevPtsGFTT[i][0]),int(prevPtsGFTT[i][1])), (int(currPtsGFTT[i][0]),int(currPtsGFTT[i][1])), color=(200, 100, 0))
+    cv2.putText(currFrameRGB, "Frame: {}".format(frameIdx), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200))
+        #cv2.putText(currFrameRGB, "Features: {}".format(len(currPts)), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200)) 
+    for i in range(len(currPtsSP)-1):
+        cv2.circle(currFrameRGB, (int(currPtsSP[i][0]),int(currPtsSP[i][1])), radius=3, color=(200, 200, 0))
+        cv2.line(currFrameRGB, (int(prevPtsSP[i][0]),int(prevPtsSP[i][1])), (int(currPtsSP[i][0]),int(currPtsSP[i][1])), color=(200, 200, 0))
+    
     cv2.imshow("Frame with keypoints", currFrameRGB)
 
 #
 # @param trackedPoints
 # @param groundtruthPoints
-def updateTrajectoryDrawing(trackedPoints, groundtruthPoints):
+def updateTrajectoryDrawing(trackedPointsGFTT, trackedPointsSP, groundtruthPoints):
     plt.cla()
-    plt.plot(trackedPoints[:,0], trackedPoints[:,2], c='blue', label="Tracking")
+    plt.plot(trackedPointsGFTT[:,0], trackedPointsGFTT[:,2], c='blue', label="Tracking GFTT")
+    plt.plot(trackedPointsSP[:,0], trackedPointsSP[:,2], c='red', label="Tracking SP")
     plt.plot(groundtruthPoints[:,0], groundtruthPoints[:,2], c='green', label="Ground truth")
     plt.title("Trajectory")
     plt.legend()
